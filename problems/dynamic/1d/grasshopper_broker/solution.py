175ce740419f5caa0631fd16813a1ea301199f86
Timofey Khirianov <tkhirianov@gmail.com>
2017-02-25 18:13:34 +0300

    Исправил решение задачи о кузнечике-брокере

--- problems/dynamic/1d/grasshopper_broker/solution.py
+++ problems/dynamic/1d/grasshopper_broker/solution.py
@@ -4,9 +4,10 @@ startup_capital = int(input())
 percents = list(map(int, input().split()))
 n = len(percents)
 
-max_money = [startup_capital, 0, startup_capital*percents[2]] + [-1]*(n-3)
+max_money = [startup_capital, 0,
+             startup_capital*(1+percents[2]/100)] + [-1]*(n-3)
 for i in range(3, n):
     money = max(max_money[i-2], max_money[i-3])
-    max_money[i] = money*(100+percents[i])/100        
+    max_money[i] = money*(1+percents[i]/100)        
 
 print(max(max_money))
