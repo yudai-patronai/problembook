7528621cdbaae3e8768b8ae2bdc6c9bb7b7d16ee
senua <etendren@gmail.com>
2017-02-27 02:46:49 +0300

    Дейкстра: использована dijkstra() из task.py

--- problems/graphs/bfs/dijkstra_len/solution.py
+++ problems/graphs/bfs/dijkstra_len/solution.py
@@ -6,23 +6,9 @@ import sys
 sys.path.append(os.path.abspath('..'))
 import task
 
-def dijkstra(G, start):
-    shortest_path = [float('+inf') for i in range(len(G))]
-    shortest_path[start] = 0
-    queue = [start]
-
-    while queue:
-        current = queue.pop(0)
-        for neighbour, weight in G[current]:
-            new_shortest_path = shortest_path[current] + weight
-            if new_shortest_path < shortest_path[neighbour]:
-                shortest_path[neighbour] = new_shortest_path
-                queue.append(neighbour)
-    return shortest_path
-
-
-def solve(G, s, f):
-    return str(dijkstra(G, s)[f]) + '\n'
+def solve(graph, x, y):
+    dist, parent = task.dijkstra(graph, x, y)
+    return '%d\n' % dist
 
 if __name__ == "__main__":
     n, m, x, y, g = task.read_task_weight()
--- problems/graphs/bfs/dijkstra_path/solution.py
+++ problems/graphs/bfs/dijkstra_path/solution.py
@@ -6,34 +6,13 @@ import sys
 sys.path.append(os.path.abspath('..'))
 import task
 
-def dijkstra(G, start):
-    shortest_path = [float('+inf') for i in range(len(G))]
-    shortest_path[start] = 0
-    queue = [start]
-    prev = [-1 for i in range(len(G))]
-    while queue:
-        current = queue.pop(0)
-        for neighbour, weight in G[current]:
-            new_shortest_path = shortest_path[current] + weight
-            if new_shortest_path < shortest_path[neighbour]:
-                shortest_path[neighbour] = new_shortest_path
-                queue.append(neighbour)
-                prev[neighbour] = current
-    return shortest_path, prev
+def solve(graph, x, y):
+    dist, parent = task.dijkstra(graph, x, y)
+    a = [y]
+    while a[0] != x:
+        a.insert(0, parent[a[0]])
 
-
-def get_shortest_path(G, start, finish):
-    _, path = dijkstra(G, start)
-    result = [finish]
-    cur = finish
-    while cur != start:
-        result.append(path[cur])
-        cur = path[cur]
-    return result[::-1]
-
-
-def solve(G, s, f):
-    return ' '.join(map(str, get_shortest_path(G, s, f))) + '\n'
+    return ' '.join(map(str, a)) + '\n'
 
 if __name__ == "__main__":
     n, m, x, y, g = task.read_task_weight()
