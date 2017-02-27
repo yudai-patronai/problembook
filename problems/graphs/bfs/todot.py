aab9c5e475a13be2f7c0c490ea814c65f740281f
senua <etendren@gmail.com>
2017-02-27 06:39:04 +0300

    todot.py: поддержка любого ввода Пропуск дополнительных параметров, поддежка взвешенных ребер.

--- problems/graphs/bfs/todot.py
+++ problems/graphs/bfs/todot.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
-n, m = map(int, input().split())
+n, m = map(int, input().split(' ', 2))
 
 print('graph x {')
 for _ in range(m):
-    a, b = input().split()
-    print('%s -- %s' % (a, b))
+    e = tuple(input().split())
+    if len(e) == 3:
+        print('%s -- %s [ label="%s" ]' % e)
+    else:
+        print('%s -- %s' % e)
 print('}')
