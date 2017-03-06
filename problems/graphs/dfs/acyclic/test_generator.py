e5631c6fcfecb84f8ddbcd2b6905ae58ec7b0b2c
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2017-03-06 22:33:56 +0300

    [graphs/dfs/acyclic] change ramdom seed

--- problems/graphs/dfs/acyclic/test_generator.py
+++ problems/graphs/dfs/acyclic/test_generator.py
@@ -5,6 +5,9 @@ import sys
 
 sys.path.append(os.path.abspath('../..'))
 import task
+import random
+
+random.seed('acyclic')
 
 tests_dir = os.path.join(os.path.dirname(__file__), 'tests')
 task.gen_tests_directed(tests_dir)
