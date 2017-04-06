98d2f648e903c4e1fb0f5b01a3221bb3d78a597c
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2017-04-06 14:12:42 +0300

    btree: исправил тесты в задачах
    Убрал тесты с повторяющимися числами из задач где написано что числа не
    повторяются.

--- problems/graphs/btree/btree_is_balanced/test_generator.py
+++ problems/graphs/btree/btree_is_balanced/test_generator.py
@@ -110,6 +110,3 @@ for i in range(1, 41):
 gen_test(tests_dir, [100])
 gen_test(tests_dir, range(100))
 gen_test(tests_dir, range(100, 0, -1))
-gen_test(tests_dir, [1] * 100)
-gen_test(tests_dir, [1, 2, 3, 4, 5] * 100)
-
