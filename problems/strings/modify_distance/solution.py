9646d5453c847e9b3cc0a4e795fb76c81073eb44
Egor Khairullin <mikari.san@gmail.com>
2017-04-19 16:49:08 +0300

    Fixed modify distance problem

--- problems/strings/modify_distance/solution.py
+++ problems/strings/modify_distance/solution.py
@@ -4,15 +4,15 @@ import sys
 import time
 
 def find_distance(first, second):
-    matrix = [[0]*len(second) for i in range(len(first))]
-    for i in range(len(first)):
+    matrix = [[0]*(len(second) + 1) for i in range(len(first) + 1)]
+    for i in range(len(first) + 1):
         matrix[i][0] = i
-    for j in range(len(second)):
+    for j in range(len(second) + 1):
         matrix[0][j] = j
 
-    for i in range(1, len(first)):
-        for j in range(1, len(second)):
-            matrix[i][j] = min(matrix[i][j - 1] + 1, matrix[i - 1][j] + 1, matrix[i - 1][j - 1] + (0 if first[i] == second[j] else 1))
+    for i in range(1, len(first) + 1):
+        for j in range(1, len(second) + 1):
+            matrix[i][j] = min(matrix[i][j - 1] + 1, matrix[i - 1][j] + 1, matrix[i - 1][j - 1] + (0 if first[i - 1] == second[j - 1] else 1))
     return matrix[-1][-1]
 
 
