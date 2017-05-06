c6b8103cf7e91b648630b42cc7c1047edfbad89a
Alexey Ermakov <fufler@gmail.com>
2017-05-06 17:33:26 +0300

    [sort/zeroes_fixed_pos] Сортировка: нули остаются на своих местах

---
+++ problems/sort/zeroes_fixed_pos/solution.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+l = list(map(int, input().split()))
+
+
+def slow(l):
+
+    n = len(l)
+
+    for i in range(n-1):
+        if l[i] == 0:
+            continue
+        m = i
+        for j in range(i, n):
+            if l[j] == 0:
+                continue
+            if l[j] < l[m]:
+                m = j
+        if m != i:
+            l[i], l[m] = l[m], l[i]
+
+    return l
+
+
+def fast(l):
+    zeroes_positions = [i for i, x in enumerate(l) if x == 0]
+    non_zeroes = sorted(x for x in l if x != 0)
+
+    zeroes_positions.append(len(l))
+    cz = 0
+    cnz = 0
+    for i in range(len(l)):
+        if i < zeroes_positions[cz]:
+            yield non_zeroes[cnz]
+            cnz += 1
+        else:
+            yield 0
+            cz += 1
+
+print(' '.join(map(str, fast(l))))
---
+++ problems/sort/zeroes_fixed_pos/test_generator.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import os
+import shutil
+import random
+
+N = 50
+MAX_L = 10000
+
+random.seed(42)
+
+tests_dir = os.path.join(os.path.dirname(__file__), 'tests')
+
+shutil.rmtree(tests_dir, ignore_errors=True)
+os.makedirs(tests_dir)
+
+for i in range(1, N+1):
+    l = random.randint(1, MAX_L)
+    if i == 1:
+        c = 0
+    elif i == 2:
+        c = l
+    else:
+        c = random.randint(0, l-1)
+
+    ans = sorted([random.randint(1, l*10) for j in range(l)])
+
+    while c > 0:
+        pos = random.randint(0, l-1)
+        if ans[pos] != 0:
+            ans[pos] = 0
+            c -= 1
+
+    with open(os.path.join(tests_dir, '{0:0>2}.a'.format(i)), 'w') as f:
+        f.write(' '.join(map(str, ans)))
+
+    positions = [k for k, x in enumerate(ans) if x > 0]
+    for j in range(len(positions)-1, 1, -1):
+        k = random.randint(0, j)
+        y = positions[j]
+        z = positions[k]
+        ans[y], ans[z] = ans[z], ans[y]
+    with open(os.path.join(tests_dir, '{0:0>2}'.format(i)), 'w') as f:
+        f.write(' '.join(map(str, ans)))
