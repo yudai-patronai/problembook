6a5cbef8be26a4217eb0154825fc30e3f5e37e6c
Vasily <vasily.ryazanov@phystech.edu>
2017-05-08 00:34:07 +0300

    [graphs/simple/edges_number_matrix] Условие задачи, решение, тесты

---
+++ problems/graphs/simple/edges_number_matrix/solution.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+def count_edges(matrix):
+    n_edge = 0
+    for i in range(n):
+        for j in range(i, n):
+            if matrix[i][j] == 1:
+                n_edge += 1
+    return n_edge
+
+
+if __name__ == "__main__":
+    n = int(input())
+    matrix = []
+    for i in range(n):
+        matrix.append(list(map(int, input().split())))
+    print(count_edges(matrix))
+
