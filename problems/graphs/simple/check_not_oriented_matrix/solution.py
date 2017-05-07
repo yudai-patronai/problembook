d3d7d175ee779f9dccb50e041c8398d3db788c21
Vasily <vasily.ryazanov@phystech.edu>
2017-05-07 21:56:38 +0300

    [graphs/simple/check_not_oriented_matrix] Условие задачи, решение, генератор тестов

---
+++ problems/graphs/simple/check_not_oriented_matrix/solution.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+def check_correct(matrix):
+    for i in range(n):
+        for j in range(i, n):
+            if matrix[i][j] != matrix[j][i]:
+                return False
+    return True
+
+
+if __name__ == "__main__":
+    n = int(input())
+    matrix = []
+    for i in range(n):
+        matrix.append(list(map(int, input().split())))
+    if check_correct(matrix):
+        print('YES')
+    else:
+        print('NO')
+
