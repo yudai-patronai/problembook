2b733fec6ff0f8cc33fdb30e54977a7108c23bd0
mikl <gertsev.mikl@gmail.com>
2017-05-09 13:35:39 +0300

    houses and shops

---
+++ problems/arrays/houses_and_shops/solution.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def solve(N,M):
+	Answer = 0
+	for i in range(N):
+	    if M[i] == "1":
+	        mind = 0
+	        while M[min(N-1,i+mind)]!="2" and M[max(0,i-mind)]!="2":
+	        	mind += 1
+	        Answer = max(Answer, mind)
+	return(Answer)
+
+
+if __name__ == "__main__":
+	print(solve(int(input()), input().split()))
---
+++ problems/arrays/houses_and_shops/test_generator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import solution
+import random
+
+def generate_answer(name, n, A):
+    with open("%s.a" % name, 'w') as f:
+        f.write(str(solution.solve(n, A)))
+
+if __name__ == "__main__":
+    test_folder = "tests"
+    shutil.rmtree(test_folder, ignore_errors=True)
+    os.mkdir(test_folder)
+    for test in range(1, 21):
+        N = 2 + random.randint(1, test // 10 * 100 + test * 10)
+        A = ["1", "2"] + [random.choice(["0", "1", "2"]) for i in range(N-2)]
+        random.shuffle(A)
+
+        test_name = os.path.join(test_folder, "%02d" % test)
+        print("generating %s..." % test_name)
+        with open(test_name, "w") as f:
+            f.write(str(N)+"\r\n")
+            f.write(" ".join(map(str,A)))
+        generate_answer(test_name, N, A)
