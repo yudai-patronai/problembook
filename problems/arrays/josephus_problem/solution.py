262097873405eb7316fcb2d73a382531628650f1
Egor Khairullin <mikari.san@gmail.com>
2017-05-16 09:40:09 +0300

    Josephus Probliem: fix input in solution and fix restrictment in statement

--- problems/arrays/josephus_problem/solution.py
+++ problems/arrays/josephus_problem/solution.py
@@ -2,13 +2,14 @@
 
 
 def solve(N,K):
-	A = [i + 1 for i in range(N)]
-	last = 0
-	while len(A) > 1:
-	    last = (last + K - 1) % len(A)
-	    A.pop(last)
-	return(A[0])  
+    A = [i + 1 for i in range(N)]
+    last = 0
+    while len(A) > 1:
+        last = (last + K - 1) % len(A)
+        A.pop(last)
+    return(A[0])
 
 
 if __name__ == "__main__":
-	print(solve(int(input()), int(input())))
+    x, y = map(int, input().split())
+    print(solve(x, y))
