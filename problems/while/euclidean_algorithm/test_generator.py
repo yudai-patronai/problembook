599f58c98e04c598e3e308be957a3d6aa970cad8
Andrey Pavlovsky <jorgiopeperoni@gmail.com>
2017-09-10 20:37:42 +0300

    Тесты для алгоритма Евклида

--- problems/while/euclidean_algorithm/test_generator.py
+++ problems/while/euclidean_algorithm/test_generator.py
@@ -7,14 +7,24 @@ from lib import random
 N = 50;
 random.seed(10000)
 
-tests_dir = os.path.join(os.path.dirname(__file__), 'tests')
+test_dir = os.path.join(os.path.dirname(__file__), 'tests')
 
-shutil.rmtree(tests_dir, ignore_errors=True)
-os.makedirs(tests_dir)
+shutil.rmtree(test_dir, ignore_errors=True)
+os.makedirs(test_dir)
 
-for i in range(1, N + 1):
-    x1 = random.randrange(1,10000)
-    x2 = random.randrange(1, 10000)
+def euclAlg(firstNumber , secondNumber):
+	temp = 0
+	while (secondNumber != 0):
+		temp = secondNumber
+		secondNumber = firstNumber % secondNumber
+		firstNumber = temp
+	return firstNumber
 
-    with open(os.path.join(tests_dir, '{0:0>2}'.format(i)), 'w') as f:
-        f.write("{0} {1}\n".format(x1, x2))
+for i in range(1, N + 1)
+	x1 = random.randrage(1,10000)
+	x2 = random.randrage(1,10000)
+
+	with open(os.path.join(test_dir, '{0:0>2}'.format(i)), 'w') as f:
+		f.write("{0} {1}\n".format(x1, x2))
+	with open(os.path.join(test_dir, '{0:0>2}.a'.format(i)), 'w') as f:
+		f.write(format(euclAlg(x1, x2)))
