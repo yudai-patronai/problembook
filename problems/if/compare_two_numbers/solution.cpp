4324658c500492bc7ca3034c3525342800a8b43d
andrew <jorgiopeperoni@gmail.com>
2017-09-10 13:52:40 +0300

    Сравнение двух чисел

---
+++ problems/if/compare_two_numbers/solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+  int firstNumber = 0;
+  int secondNumber = 0;
+
+  std::cin >> firstNumber >> secondNumber;
+
+  if (firstNumber > secondNumber)
+    std::cout << 1 << std::endl;
+  else if (firstNumber < secondNumber)
+    std::cout << 2 << std::endl;
+  else
+    std::cout << 0 << std::endl;
+
+  return 0;
+}
