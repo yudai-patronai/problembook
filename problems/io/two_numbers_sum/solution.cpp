54f31ddfaec55d9eb28d9024f259ef0d9fe3b395
andrew <jorgiopeperoni@gmail.com>
2017-09-10 13:34:51 +0300

    Исправлен тег и формат имени

---
+++ problems/io/two_numbers_sum/solution.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+  int firstNumber = 0;
+  int secondNumber = 0;
+
+  std::cin >> firstNumber >> secondNumber;
+
+  std::cout << firstNumber + secondNumber << std::endl;
+
+  return 0;
+}
