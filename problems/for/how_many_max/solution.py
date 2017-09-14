dcf85083f76b1c6459cdf10e78a5b0b757cc6f83
Alexander Smolyakov <sashasmolykov@gmail.com>
2017-09-15 01:50:45 +0300

    Количество элементов, равных максимуму

---
+++ problems/for/how_many_max/solution.py
@@ -0,0 +1,14 @@
+number = -1
+current_max = -1
+dct = dict()
+
+while number != 0:
+	number = int(input())
+
+	dct[number] = dct.get(number, 0) + 1
+
+	if number > current_max:
+		current_max = number
+
+print(dct[current_max])
+
