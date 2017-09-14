9a30e9e9e58bc6a517f576846d469ecdffed3098
anton.k <anton.nail@gmail.com>
2017-09-15 02:45:04 +0300

    [cpp course] 3 week - 8 task: Быстрая сортировка #24

---
+++ problems/arrays/qsort/solution.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+void sort(int *array, int begin, int end)
+{
+	int hi = begin;
+	int ti = end;
+
+	int pivot = array[ti];
+	while (hi <= ti)
+	{
+		while (array[hi] < pivot)
+			++hi;
+
+		while (array[ti] > pivot)
+			--ti;
+
+		if (hi <= ti)
+		{
+			int tmp = array[hi];
+			array[hi] = array[ti];
+			array[ti] = tmp;
+
+			++hi;
+			--ti;
+		}
+	}
+
+	if (ti > begin)
+		sort(array, begin, ti);
+	if (hi < end)
+		sort(array, hi , end);
+}
+
+int main()
+{
+	int N;
+	std::cin >> N;
+
+	int* array = new int[N];
+
+	for (int i = 0; i < N; ++i)
+	{
+		std::cin >> array[i];
+	}
+
+	sort(array, 0, N - 1);
+
+	for (int i = 0; i < N; ++i)
+	{
+		std::cout << array[i] << " ";
+	}
+	std::cout << std::endl;
+
+	return 0;
+}
