853befb30e090d17345dee0064a1acb84dfc4a60
Maxim Khlamov <mkhlamov@gmail.com>
2017-09-14 23:25:57 +0300

    [3 week - 6 task: Длина последовательности] Условие, генератор тестов, решение

---
+++ problems/while/sequence_length/solution.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    length = 0
+    num = int(input())
+    while num != 0:
+        length += 1
+        num = int(input())
+    print(length)
---
+++ problems/while/sequence_length/test_generator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import os
+from lib import random
+import shutil
+
+random.seed(42)
+
+
+def generate_answer(name, l):
+    with open("%s.a" % name, 'w') as f:
+        f.write(str(l))
+
+
+def generate_test(name):
+    length = random.randint(1, 100)
+    seq = [random.randint(1, 100) for i in range(length)]
+    tail = [0] + [random.randint(1, 100) for i in range(random.randint(0, 3))]
+    seq += tail
+    with open(name, "w") as f:
+        for i in seq:
+            f.write(str(i) + '\n')
+    generate_answer(name, length)
+
+
+if __name__ == "__main__":
+    test_folder = "tests"
+    shutil.rmtree(test_folder, ignore_errors=True)
+    os.mkdir(test_folder)
+    for test in range(1, 71):
+        test_name = os.path.join(test_folder, "%02d" % test)
+        print("generating %s..." % test_name)
+        generate_test(test_name)
