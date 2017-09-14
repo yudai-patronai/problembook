ce2d195cf8f96e17233fe55ca2e68e766964ef3c
Maxim Abramov <abramov@visillect.com>
2017-09-15 00:37:33 +0300

    Добавлена задача "Сумма цифр трехзначного числа"

---
+++ problems/if/sum_of_digits/solution.py
@@ -0,0 +1,5 @@
+a = list(map(lambda x: int(x), input()))
+sum = 0
+for i in a:
+	sum += i
+print(sum)
---
+++ problems/if/sum_of_digits/test_generator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import os
+from lib import random
+import shutil
+
+N = 50
+
+random.seed(37)
+
+tests_dir = os.path.join(os.path.dirname(__file__), 'tests')
+
+shutil.rmtree(tests_dir, ignore_errors=True)
+os.makedirs(tests_dir)
+
+for i in range(1, N + 1):
+    num = random.randint(100, 999)
+    ans = num // 100 + (num % 100) // 10 + num % 10
+
+    with open(os.path.join(tests_dir, '{0:0>2}'.format(i)), 'w') as f:
+        f.write(str(num))
+    with open(os.path.join(tests_dir, '{0:0>2}.a'.format(i)), 'w') as f:
+        f.write(str(ans))
+
