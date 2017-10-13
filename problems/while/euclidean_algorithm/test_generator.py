fba1e2be262af1875bff381b1e51a235924500e4
vasyarv <vasily.ryazanov@phystech.edu>
2017-10-13 11:07:57 +0300

    euclidian algorithm: partial case

--- problems/while/euclidean_algorithm/test_generator.py
+++ problems/while/euclidean_algorithm/test_generator.py
@@ -5,7 +5,7 @@ import shutil
 
 #from lib import random
 import random
-N = 50
+N = 15
 random.seed(10000)
 
 test_dir = os.path.join(os.path.dirname(__file__), 'tests')
@@ -25,8 +25,12 @@ def eucl_alg(firstNumber, secondNumber):
 
 
 for i in range(1, N + 1):
-    x1 = random.randrange(1, 10000)
-    x2 = random.randrange(1, 10000)
+    if i == 1:
+        x1 = 15
+        x2 = 1
+    else:
+        x1 = random.randrange(1, 10000)
+        x2 = random.randrange(1, 10000)
 
     with open(os.path.join(test_dir, '{0:0>2}'.format(i)), 'w') as f:
         f.write("{0}\n{1}\n".format(x1, x2))
