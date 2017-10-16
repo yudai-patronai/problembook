95688413446eadaf57d1caac3b579f7a0b08aa64
anton.k <anton.nail@gmail.com>
2017-10-17 01:32:44 +0300

    [strings/fence] Заборчик #219
    Fix compiler warning

--- problems/strings/fence/solution.cpp
+++ problems/strings/fence/solution.cpp
@@ -7,7 +7,7 @@ int main()
   std::getline(std::cin, input);
 
   int counter = 0;
-  for (int i = 0; i < input.size(); ++i)
+  for (size_t i = 0; i < input.size(); ++i)
   {
     if (input.at(i) == ' ')
       continue;
