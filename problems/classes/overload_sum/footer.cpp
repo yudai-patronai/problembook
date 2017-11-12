f73b89744fabd5e05582dfa04ac4293e7002be72
Vasili Kovalyov <kvhq@Vasilis-MacBook-Pro.local>
2017-11-13 02:01:04 +0300

    [11w-4e] full problem mipt-cs/problembook#332

---
+++ problems/classes/overload_sum/footer.cpp
@@ -0,0 +1,24 @@
+int main() {
+    MyClass class1;
+    MyClass class2;
+    int numberOfTests;
+    std::cin >> numberOfTests;
+    for (int i = 0; i < numberOfTests; ++i) {
+        int number;
+        std::string word;
+        std::cin >> number;
+        std::cin >> word;
+        class1.AddElement(number, word);
+    }
+    std::cin >> numberOfTests;
+    for (int i = 0; i < numberOfTests; ++i) {
+        int number;
+        std::string word;
+        std::cin >> number;
+        std::cin >> word;
+        class2.AddElement(number, word);
+    }
+    class1 + class2;
+    class1.PrintStructures();
+    return 0;
+}
