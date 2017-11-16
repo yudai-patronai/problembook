2d12fe80c0d00bf4077776a97619a46a7a30d7aa
Maxim Khlamov <mkhlamov@gmail.com>
2017-11-16 11:49:21 -0800

    [py_sem1_w12_t5_v2] добавлены тесты по умолчанию

--- problems/dynamic/1d/containerABC/test_generator.py
+++ problems/dynamic/1d/containerABC/test_generator.py
@@ -6,7 +6,7 @@ import shutil
 from solution import solve
 
 random.seed(42)
-MAX_N = 20
+MAX_N = 25
 
 def generate_test(name, n):
     with open(name, "w") as f:
@@ -19,11 +19,13 @@ if __name__ == "__main__":
     test_folder = "tests"
     shutil.rmtree(test_folder, ignore_errors=True)
     os.mkdir(test_folder)
-    questions = random.sample(range(1, MAX_N), 15)
+
+    questions = [2, 10, 3, 7, 8]
+    questions.extend(random.sample(range(11, MAX_N), 10))
     if MAX_N not in questions:
         questions[-1] = MAX_N
 
     for test, n in enumerate(questions):
-        test_name = os.path.join(test_folder, "%02d" % test)
+        test_name = os.path.join(test_folder, "%02d" % (test+1))
         print("generating %s..." % test_name)
         generate_test(test_name, n)
