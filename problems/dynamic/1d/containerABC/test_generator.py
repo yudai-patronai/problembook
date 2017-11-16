ad9e288e6a44d29050d19b18b6509907bcf68482
Maxim Khlamov <mkhlamov@gmail.com>
2017-11-16 10:33:17 -0800

    [py_sem1_w12_t5_v2] Добавлена задача про контейнеры A, B, C

---
+++ problems/dynamic/1d/containerABC/test_generator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os
+from lib import random
+import shutil
+from solution import solve
+
+random.seed(42)
+MAX_N = 20
+
+def generate_test(name, n):
+    with open(name, "w") as f:
+        f.write(str(n) + '\n')
+    #answer
+    with open("%s.a" % name, 'w') as f:
+        f.write(str(solve(n)))
+
+if __name__ == "__main__":
+    test_folder = "tests"
+    shutil.rmtree(test_folder, ignore_errors=True)
+    os.mkdir(test_folder)
+    questions = random.sample(range(1, MAX_N), 15)
+    if MAX_N not in questions:
+        questions[-1] = MAX_N
+
+    for test, n in enumerate(questions):
+        test_name = os.path.join(test_folder, "%02d" % test)
+        print("generating %s..." % test_name)
+        generate_test(test_name, n)
