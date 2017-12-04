8ff8a15c7f71610f9374c6fb0f3355b08fe2fdb8
Maxim Abramov <maksim.abramov@phystech.edu>
2017-12-04 23:07:00 +0300

    [sort/livejournal] Сделан генератор тестов

--- problems/sort/livejournal/test_generator.py
+++ problems/sort/livejournal/test_generator.py
@@ -7,31 +7,52 @@ from lib import random
 random.seed(42)
 
 
+def get_name():
+    return ''.join(random.sample([chr(i) for i in range(ord('a'), ord('z')+1)]+['-'], random.randint(1, 20)))
+
+
 def generate_test(test_file):
     n = random.randint(1, 10000)
-    a = [random.randint(1, 100000) for _ in range(n)]
-    b = sorted(a)
-    s = 0
-    for i in range(n // 2 + 1):
-        s += b[i] // 2 + 1
-    with open(test_file, 'w') as f:
-        f.write(str(n) + '\n')
-        f.write(' '.join(map(str, a)))
-    with open(test_file + '.a', 'w') as f:
-        f.write(str(s) + '\n')
+    f = [get_name() for _ in range(n)]
+    m = random.randint(1, 10000)
+    af = [get_name() for _ in range(m)]
+    mf = []
+    with open(test_file, 'w') as fs:
+        fs.write(str(n) + '\n')
+        for i in f:
+            fs.write(i + '\n')
+        fs.write(str(m) + '\n')
+        for i in af:
+            fs.write(i + '\n')
+    for i in list(f):
+        if i in af:
+            mf.append(i)
+            af.remove(i)
+    with open(test_file + '.a', 'w') as fs:
+        fs.write("Friends: " + ', '.join(sorted(f)) + '\n')
+        fs.write("Mutual Friends: " + ', '.join(sorted(mf)) + '\n')
+        fs.write("Also Friend of: " + ', '.join(sorted(af)) + '\n')
 
 
-def write_test(test_file, a):
-    n = len(a)
-    b = sorted(a)
-    s = 0
-    for i in range(n // 2 + 1):
-        s += b[i] // 2 + 1
-    with open(test_file, 'w') as f:
-        f.write(str(n) + '\n')
-        f.write(' '.join(map(str, a)))
-    with open(test_file + '.a', 'w') as f:
-        f.write(str(s) + '\n')
+def write_test(test_file, f, af):
+    n = len(f)
+    m = len(af)
+    mf = []
+    with open(test_file, 'w') as fs:
+        fs.write(str(n) + '\n')
+        for i in f:
+            fs.write(i + '\n')
+        fs.write(str(m) + '\n')
+        for i in af:
+            fs.write(i + '\n')
+    for i in list(f):
+        if i in af:
+            mf.append(i)
+            af.remove(i)
+    with open(test_file + '.a', 'w') as fs:
+        fs.write("Friends: " + ', '.join(sorted(f)) + '\n')
+        fs.write("Mutual Friends: " + ', '.join(sorted(mf)) + '\n')
+        fs.write("Also Friend of: " + ', '.join(sorted(af)) + '\n')
 
 
 if __name__ == '__main__':
@@ -39,11 +60,12 @@ if __name__ == '__main__':
     shutil.rmtree(test_folder, ignore_errors=True)
     os.mkdir(test_folder)
 
-    write_test(os.path.join(test_folder, "01"), [1])
-    write_test(os.path.join(test_folder, "02"), [158])
-    write_test(os.path.join(test_folder, "03"), [1, 1, 1, 1, 1, 1, 1])
-    write_test(os.path.join(test_folder, "04"), [2, 45, 7, 6, 33])
-    write_test(os.path.join(test_folder, "05"), [random.randint(1, 100000) for _ in range(10000)])
+    write_test(os.path.join(test_folder, "01"), ["vasya"], ["petya"])
+    write_test(os.path.join(test_folder, "02"), [], [])
+    write_test(os.path.join(test_folder, "03"), ["name1", "name2", "name3"], [])
+    write_test(os.path.join(test_folder, "04"), [], ["name4", "name5"])
+    write_test(os.path.join(test_folder, "05"), ["a", "b", "c", "d"], ["d", "c", "b", "a"])
+    write_test(os.path.join(test_folder, "06"), [get_name() for _ in range(10000)], [get_name() for _ in range(10000)])
 
-    for test in range(6, 10):
+    for test in range(7, 8):
         generate_test(os.path.join(test_folder, "0{}".format(test)))
