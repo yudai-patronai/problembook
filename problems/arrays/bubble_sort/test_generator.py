b19933bc12bd6d6e84af2302a5f28518faffaf26
senya <etendren@gmail.com>
2017-12-23 14:48:02 +0300

    arrays: исправил опечатки в коде генераторов
    vasya_birthday, bubble_sort, index_elements_sum - не запускаются.
    Исправил опечатки, логику не проверял.
    
    arrays/bubble_sort/test_generator: не достает скобки
    
    arrays/index_elements_sum/test_generator: ошибки в коде
    
    Исправил ошибки в коде
    
    fix_bubble_sort

--- problems/arrays/bubble_sort/test_generator.py
+++ problems/arrays/bubble_sort/test_generator.py
@@ -17,8 +17,8 @@ for i in range(1, NUM_TEST + 1):
     for j in range(n):
         array.append(random.randint(1, 1000))
 
-    with open(os.path.join(tests_dir, '{0:0>2}'.format(i)), 'w') as f:
-        f.write('{0}\n{1}\n'.forman(n, ' '.join(str(val) for val in array)))
+    with open(os.path.join(test_dir, '{0:0>2}'.format(i)), 'w') as f:
+        f.write('{0}\n{1}\n'.format(n, ' '.join(str(val) for val in array)))
 
     with open(os.path.join(test_dir, '{0:0>2}.a'.format(i)), 'w') as f:
-        f.write('{0}\n'.format(sorted(array))
+        f.write('{0}\n'.format(sorted(array)))
