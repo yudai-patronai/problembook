54d8784914cc2f57663e1f6b622731eac44aad85
senya <etendren@gmail.com>
2018-01-04 08:19:40 +0300

    contest.py: вернул потерянный статус задачи в валидации
    status был (видимо случайно) удален в комите c7ee345dbd5a

--- contest.py
+++ contest.py
@@ -723,7 +723,7 @@ def validate(params):
     report = []
     failed = False
     for k, (prob, tests, unique_tests, test_generator, solution, checksum, status) in enumerate(result):
-        report.append([k+1, prob.id, prob.longname, tests, unique_tests, test_generator, solution, checksum])
+        report.append([k+1, prob.id, prob.longname, tests, unique_tests, test_generator, solution, checksum, status])
 
         failed = failed or (prob.errors and not prob.fixme)
 
