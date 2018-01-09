8d113395091c96934d33bc8d1921f2be57437efd
AntilMB <sashasmolykov@gmail.com>
2018-01-09 14:07:19 +0300

    стилистические правки кода в problems/dynamic/1d/max_subseq

--- problems/dynamic/1d/max_subseq/solution.py
+++ problems/dynamic/1d/max_subseq/solution.py
@@ -1,13 +1,13 @@
 def solution(arr):
     N = len(arr)
-    subseq_len = []
+    sseq_len = []
     for i in range(N):
-        subseq = [el for index, el in enumerate(subseq_len) if arr[index] < arr[i]]
+        subseq = [el for idx, el in enumerate(sseq_len) if arr[idx] < arr[i]]
         if subseq:
-            subseq_len.append(max(subseq) + 1)
+            sseq_len.append(max(subseq) + 1)
         else:
-            subseq_len.append(1)
-    return max(subseq_len)
+            sseq_len.append(1)
+    return max(sseq_len)
 
-arr = [int(i) for i in input().strip().split(' ')] 
+arr = [int(i) for i in input().strip().split(' ')]
 print(solution(arr))
