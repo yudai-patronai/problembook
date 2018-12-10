2c0245f41fd132a64886fdb0db1e97ffe0b9dee6
AntilMB <sashasmolykov@gmail.com>
2018-12-10 15:22:54 +0300

    fix problem with list_compehension

--- problems/arrays/list_comprehension/footer.py
+++ problems/arrays/list_comprehension/footer.py
@@ -5,7 +5,7 @@ result = list_filter(map(int, input().strip().split()))
 
 assert not re.findall(r'append', _foo), 'append запрещен'
 assert not re.findall(r'extend', _foo), 'extend запрещен'
-assert not re.findall(r'+', _foo), 'not bad, но не прокатит')
+assert not re.findall(r'\+', _foo), 'not bad, но не прокатит'
 assert isinstance(list_filter([20, 5, 3]), list), 'надо вернуть list'
 
 print(*result)
