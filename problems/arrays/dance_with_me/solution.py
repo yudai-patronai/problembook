fb0266bf0c3a4547feed9d1e74a8471197ea062c
antilmb <sashasmolykov@gmail.com>
2018-12-13 12:17:22 +0300

    fix itertools task

--- problems/arrays/dance_with_me/solution.py
+++ problems/arrays/dance_with_me/solution.py
@@ -1,9 +1,16 @@
-from itertools import product
+from itertools import product, combinations, permutations
 
+p, q = [int(x) for x in input().strip().split()]
 names = input().strip().split()
 
-men = [x for x in names if x[-1] == 'm']
-women = [x for x in names if x[-1] == 'w']
+
+def grouper(seq, last, value):
+    men = [x for x in seq if x[-1] == last]
+    return ['+'.join(x) for x in permutations(men, value)]
+
+
+men = grouper(names, 'm', p)
+women = grouper(names, 'w', q)
 
 pairs = ['{}_{}'.format(x, y) for x, y in product(men, women)]
 pairs.sort()
