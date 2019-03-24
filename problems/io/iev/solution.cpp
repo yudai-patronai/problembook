c90f45a33d2d49d5f2897df7010d75d96df7ba6d
Stiimo <maksimkzn@mail.ru>
2019-03-24 17:55:52 +0300

    Fixed iev problem

--- problems/io/iev/solution.cpp
+++ problems/io/iev/solution.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <iomanip>
 
 int main() {
-    double a;
+    double a = 0;
     double p[] = {1.0, 1.0, 1.0, 0.75, 0.5, 0.0};
     for (int i = 0; i < 6; ++i) {
         int k;
         std::cin >> k;
         a += k * p[i];
     }
-    std::cout << a << '\n';
+    std::cout << std::fixed << a << '\n';
     return 0;
 }
