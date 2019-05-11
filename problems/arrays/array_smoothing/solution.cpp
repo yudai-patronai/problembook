b4b2e10f5195b2a9340a21b64d2bebf87f2743ee
Alexey Ermakov <fufler@gmail.com>
2019-05-11 16:54:22 +0300

    [arrays/array_smoothing] Слегка поправил условие и решение

--- problems/arrays/array_smoothing/solution.cpp
+++ problems/arrays/array_smoothing/solution.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
+#include <cmath>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::floor;
+using std::swap;
 
 int main() {
     int n, k;
-    std::cin >> n >> k;
+
+    cin >> n >> k;
+
     int *a = new int[n];
-    for (int i = 0; i < n; i++) {
-        std::cin >> a[i];
-    }
     int *b = new int[n];
+
+    for (int i = 0; i < n; i++)
+        std::cin >> a[i];
+
     for (int j = 0; j < k; j++) {
-        for (int i = 0; i < n; i++) {
-            b[i] = static_cast<int>((a[(i - 1 + n) % n] + a[i]
-                                     + a[(i + 1) % n]) / 3.0);
-        }
-        for (int i = 0; i < n; i++) {
-            a[i] = b[i];
-        }
+        for (int i = 0; i < n; i++)
+            b[i] = floor((a[(i - 1 + n) % n] + a[i] + a[(i + 1) % n]) / 3.0);
+
+        swap(a, b);
     }
-    for (int i = 0; i < n; i++) {
-        std::cout << a[i] << ' ';
-    }
+
+    for (int i = 0; i < n; i++)
+        cout << a[i] << ' ';
+
+    cout << endl;
+
     delete[] a;
     delete[] b;
+
     return 0;
 }
