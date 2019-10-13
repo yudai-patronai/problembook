aa509bbcd37a450a316916c5811881e272400180
leonid <puleon@mail.ru>
2019-10-14 00:17:18 +0300

    fix: add cyclic shift by N positions by Leonid Pugachev.

--- problems/arrays/cycle_shift_N/solution.py
+++ problems/arrays/cycle_shift_N/solution.py
@@ -1,6 +1,7 @@
 def cycle_shift_N(arr, N, M):
-    tmp = arr[0]
-    for i in range(0,  M * (N - 1), M):
-        arr[i % N] = arr[(i + M) % N]
-    arr[M * (N-1) % N] = tmp
-
+    M = M % N
+    for i in range(M):
+        tmp = arr[0]
+        for idx in range(1, N):
+            arr[idx - 1] = arr[idx]
+        arr[-1] = tmp
--- problems/arrays/cycle_shift_N/test_generator.py
+++ problems/arrays/cycle_shift_N/test_generator.py
@@ -4,13 +4,15 @@ from copy import copy
 
 RAND_TESTS_NUM = 20
 MAX_RAND_NUM = 1000
-MAX_RAND_RANGE = 100
+MAX_RAND_RANGE = 10
 
 def cycle_shift_N(arr, N, M):
-    tmp = arr[0]
-    for i in range(0,  M * (N - 1), M):
-        arr[i % N] = arr[(i + M) % N]
-    arr[M * (N-1) % N] = tmp
+    M = M % N
+    for i in range(M):
+        tmp = arr[0]
+        for idx in range(1, N):
+            arr[idx - 1] = arr[idx]
+        arr[-1] = tmp
 
 def get_case(arr, N, M):
   arr_orig = copy(arr)
@@ -23,7 +25,7 @@ tests.add(*get_case([1, 2, 3, 4, 5], 5, 3))
 tests.add(*get_case([1], 1, 10))
 
 for _ in range(RAND_TESTS_NUM):
-  N = randrange(MAX_RAND_RANGE)
+  N = randrange(1, MAX_RAND_RANGE)
   arr = [randint(MAX_RAND_NUM) for _ in range(N)]
   M = randrange(MAX_RAND_RANGE)
   tests.add(*get_case(arr, N, M))
