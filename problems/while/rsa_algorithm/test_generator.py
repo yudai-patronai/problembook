6634b8295e2adb0f13a31679b03add6cb5a2b732
leonid <puleon@mail.ru>
2019-10-13 15:40:39 +0300

    feat: add RSA algorithm by Leonid Pugachev

--- problems/while/rsa_algorithm/test_generator.py
+++ problems/while/rsa_algorithm/test_generator.py
@@ -1,5 +1,5 @@
 from lib.testgen import TestSet
-from lib.random import randint
+from lib.random import randint, randrange
 
 RAND_TESTS_NUM = 20
 MAX_RAND_NUM = 1000
@@ -22,6 +22,6 @@ tests.add(*get_case([1, 2, 3, 4, 5]))
 tests.add(*get_case([75]))
 
 for _ in range(RAND_TESTS_NUM):
-  M = randint.randrange(MAX_RAND_RANGE)
+  M = randrange(MAX_RAND_RANGE)
   mess = [randint(MAX_RAND_NUM) for _ in range(M)]
   tests.add(*get_case(mess))
