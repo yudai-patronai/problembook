ab48e65c64544f435d2149147527f76b887a1507
Степан Захаров <stepanzh@gmail.com>
2019-10-15 13:45:04 +0300

    [how_many_min] поправил тесты

--- problems/while/how_many_min/test_generator.py
+++ problems/while/how_many_min/test_generator.py
@@ -16,6 +16,13 @@ for num in range(1, N + 1):
 
 	min_in_cur_seq = random.randint(1, 200)
 	max_in_cur_seq = random.randint(1, 200)
+
+	if max_in_cur_seq < min_in_cur_seq:
+		min_in_cur_seq, max_in_cur_seq = max_in_cur_seq, min_in_cur_seq
+
+	if min_in_cur_seq == max_in_cur_seq:
+		max_in_cur_seq += 1
+
 	seq = [random.randint(min_in_cur_seq, max_in_cur_seq) for i in range(random.randint(1, 10000))]
 	for i in seq:
 		dct[i] = dct.get(i, 0) + 1
@@ -26,5 +33,5 @@ for num in range(1, N + 1):
 		f.write("\n".join(map(str, seq)))
 
 	with open(os.path.join(tests_dir, '{0:0>2}.a'.format(num)), 'w') as f:
-		#f.write(str(dct[max(seq)]))
-		f.write(str(dct[max(seq)]))
+		seq.pop()  # pop zero from the end
+		f.write(str(dct[min(seq)]))
