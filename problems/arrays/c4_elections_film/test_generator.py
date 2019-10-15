06ec179264c50861ca157053a349e40c2ada15a9
Степан Захаров <stepanzh@gmail.com>
2019-10-15 20:32:19 +0300

    [arrays/c4_elections_film] сменил кириллицу на латиницу - ежадж ругается

--- problems/arrays/c4_elections_film/test_generator.py
+++ problems/arrays/c4_elections_film/test_generator.py
@@ -10,29 +10,29 @@ def lstrip_all(text):
 tests = TestSet()
 
 tests.add(lstrip_all("""6
-                     Белое солнце пустыни
-                     Бриллиантовая рука
-                     Белое солнце пустыни
-                     Белое солнце пустыни
-                     Гараж
-                     Бриллиантовая рука"""),
-          lstrip_all("""Белое солнце пустыни 3
-                     Бриллиантовая рука 2
-                     Гараж 1"""))
+                     Beloe solnce pustyni
+                     Brilliantovaya ruka
+                     Beloe solnce pustyni
+                     Beloe solnce pustyni
+                     Garazh
+                     Brilliantovaya ruka"""),
+          lstrip_all("""Beloe solnce pustyni 3
+                     Brilliantovaya ruka 2
+                     Garazh 1"""))
 
 tests.add(lstrip_all("""3
-                     Разум и чувства
-                     Гордость и предубеждение
-                     Гордость и предубеждение"""),
-          lstrip_all("""Гордость и предубеждение 2
-                     Разум и чувства 1"""))
+                     Razum i chuvstva
+                     Gordost' i predubezhdenie
+                     Gordost' i predubezhdenie"""),
+          lstrip_all("""Gordost' i predubezhdenie 2
+                     Razum i chuvstva 1"""))
 
 tests.add(lstrip_all("""5
-                     Сноуден
-                     Сноуден
-                     Сноуден
-                     Сноуден
-                     Сноуден"""),
-          "Сноуден 5")
+                     Snouden
+                     Snouden
+                     Snouden
+                     Snouden
+                     Snouden"""),
+          "Snouden 5")
 
 tests.add("100\n" + "A\n"*49 + "B\n"*51, "B 51\nA 49\n")
