7b194a92a80503160c197fe009da5b02b7a65beb
rodin-vv <rodin-vv@yandex-team.ru>
2019-10-20 19:10:33 +0300

    Добавлена задача на реализацию сортировки пузырьком

--- problems/sort/bubble_sort/solution.py
+++ problems/sort/bubble_sort/solution.py
@@ -7,6 +7,7 @@ def bubbleSort(arrayToSort):
         for j in range(0, n - i - 1):
             if arrayToSort[j] > arrayToSort[j + 1] :
                 arrayToSort[j], arrayToSort[j + 1] = arrayToSort[j + 1], arrayToSort[j]
+                print(' '.join(map(str, arrayToSort)))
 
 inputArray = [int(element) for element in input().split()]
 bubbleSort(inputArray)
