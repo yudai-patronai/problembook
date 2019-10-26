1259dd41dec5944bc809fb9510e62bd1a2b3efb1
Elizaveta A Lyapina <55664188+ealyapina@users.noreply.github.com>
2019-10-26 16:05:05 +0300

    recursion/money_exchange починила ввод данных

--- problems/recursion/money_exchange/footer.py
+++ problems/recursion/money_exchange/footer.py
@@ -6,7 +6,12 @@ for pattern in exclude_patterns:
     assert not re.findall(reobj, source_code), 'instruction "{}" could not be used'.format(pattern)
 exec(source_code)  
     
-
+    money = int(input())
+    coinstr= input()
+    if coinstr !='':
+        coins = [int(x) for x in coinstr.split(" ")]
+    else:
+        coins = []
     ans = make_exchange(money, coins)
     
     
