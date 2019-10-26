b95573a452bdc7775b3ba8b98d6398b3478fd5d8
Vladimir Markin <vmadrid57@gmail.com>
2019-10-26 17:39:33 +0300

    commit d7f8d30b-8d20-46b4-84e3-7e0bf7e38932 Author: Vladimir Markin <vmadrid57@gmail.com> Date    26 Oct 2019 	recursion/1a  переработал задачу

--- problems/recursion/1a/test_generator.py
+++ problems/recursion/1a/test_generator.py
@@ -1,37 +1,16 @@
 #!/usr/bin/env python3
 
-import os
-import shutil
+from lib.testgen import TestSet
 
+tests = TestSet()
+tests.add('0\n', '1 0\n')
+tests.add('1\n', '1 0\n')
+tests.add('6\n', '4 4\n')
+tests.add('10\n', '6 6\n')
+tests.add('14\n', '9 9\n')
+tests.add('20\n', '13 12\n')
+tests.add('25\n', '16 16\n')
+tests.add('30\n', '19 19\n')
+tests.add('77\n', '48 48\n')
+tests.add('37\n', '23 23\n')
 
-def make_test(test_file, n, ans_array):
-
-    with open(test_file, 'w') as f:
-        f.write(str(n) + '\n')
-
-    with open(test_file + '.a', 'w') as f:
-        for elem in ans_array:
-            f.write(str(elem) + ' ')
-        f.write('\n')
-
-if __name__ == '__main__':
-
-    test_folder = 'tests'
-    shutil.rmtree(test_folder, ignore_errors=True)
-    os.mkdir(test_folder)
-
-    boards_ans = {
-        '01': (0, [1, 0]),
-        '02': (1, [1, 0]),
-        '03': (6, [4, 4]),
-        '04': (10, [6, 6]),
-        '05': (14, [9, 9]),
-        '06': (20, [13, 12]),
-        '07': (25, [16, 16]),
-        '08': (30, [19, 19]),
-        '09': (77, [48, 48]),
-        '10': (37, [23, 23]),
-    }
-
-    for key, value in boards_ans.items():
-        make_test(os.path.join(test_folder, key), value[0], value[1])
