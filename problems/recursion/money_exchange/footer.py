e5a1925d2cb053e80b781d4d43a899ba3a747d05
Elizaveta A Lyapina <55664188+ealyapina@users.noreply.github.com>
2019-10-26 16:50:12 +0300

    [recursion/money_exchange] indent corr

--- problems/recursion/money_exchange/footer.py
+++ problems/recursion/money_exchange/footer.py
@@ -6,13 +6,11 @@ for pattern in exclude_patterns:
     assert not re.findall(reobj, source_code), 'instruction "{}" could not be used'.format(pattern)
 exec(source_code)  
     
-    money = int(input())
-    coinstr= input()
-    if coinstr !='':
-        coins = [int(x) for x in coinstr.split(" ")]
-    else:
-        coins = []
-    ans = make_exchange(money, coins)
-    
-    
-    print(ans)
+money = int(input())
+coinstr= input()
+if coinstr !='':
+    coins = [int(x) for x in coinstr.split(" ")]
+else:
+    coins = []
+ans = make_exchange(money, coins)
+print(ans)
