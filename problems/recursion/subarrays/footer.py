b169d69b67fb967196bdc18df2267201eadac016
Степан Захаров <stepanzh@gmail.com>
2019-10-27 14:45:04 +0300

    [recursion/subarrays] добавил проверку for и while

---
+++ problems/recursion/subarrays/footer.py
@@ -0,0 +1,15 @@
+''' # source_code переменная конец
+
+# валидация на содержание запрещённых инструкций
+exclude_patterns = ['for', 'while']
+
+for pattern in exclude_patterns:
+    reobj = re.compile(pattern)
+    assert not re.findall(reobj, source_code), 'instruction "{}" could not be used'.format(pattern)
+
+
+exec(source_code)
+
+# считывание входных данных
+arr = input().split()
+printSubArrays(arr, 0, 0) 
