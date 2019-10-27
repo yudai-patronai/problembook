77709f6a262fa7072d0e661d63420956757becaa
Степан Захаров <stepanzh@gmail.com>
2019-10-27 14:44:31 +0300

    [recursion/subarrays] удалил считывание данных

--- problems/recursion/subarrays/solution.py
+++ problems/recursion/subarrays/solution.py
@@ -2,16 +2,10 @@ def printSubArrays(arr, start, end):
 	#stop at the end	
 	if end == len(arr):
 		return
-# Increment the end point and start from 0 
+	# Increment the end point and start from 0 
 	elif start > end:
 		return printSubArrays(arr, 0, end + 1) 
-# Print the subarray and increment start
+	# Print the subarray and increment start
 	else:
-		
 		print(*arr[start:end + 1])
 		return printSubArrays(arr, start + 1, end) 
-
-if __name__=="__main__":
-	arr=input().split()
-	printSubArrays(arr, 0, 0) 
-
