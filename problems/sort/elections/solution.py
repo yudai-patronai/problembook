33d34eed2c467b41feea5ea63b305a650caafa36
Alexander Kazantsev <kazantsev@phystech.edu>
2019-11-01 13:53:44 +0300

    [sort/elections] refactoring of test generator, statement.md became more clear, removed 'sorted' from solution.py

--- problems/sort/elections/solution.py
+++ problems/sort/elections/solution.py
@@ -1,5 +1,14 @@
+def bubble_sort(arr_iter):
+    arr = list(arr_iter)
+    for i in range(len(arr) - 1, 0, -1):
+        for j in range(i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
 n = int(input())
-a = sorted(list(map(int, input().split())))
+a = bubble_sort(map(int, input().split()))
 s = 0
 for i in range(n // 2 + 1):
     s += a[i] // 2 + 1
--- problems/sort/elections/test_generator.py
+++ problems/sort/elections/test_generator.py
@@ -1,49 +1,32 @@
-#!/usr/bin/env python3
-
-import os
-import shutil
-from lib import random
-
-random.seed(42)
+from lib.testgen import TestSet
+from lib.random import seed, randint
 
 
-def generate_test(test_file):
-    n = random.randint(1, 10000)
-    a = [random.randint(1, 100000) for _ in range(n)]
-    b = sorted(a)
-    s = 0
-    for i in range(n // 2 + 1):
-        s += b[i] // 2 + 1
-    with open(test_file, 'w') as f:
-        f.write(str(n) + '\n')
-        f.write(' '.join(map(str, a)))
-    with open(test_file + '.a', 'w') as f:
-        f.write(str(s) + '\n')
+def solution(arr):
+    b = sorted(arr)
+    return sum(b[i] // 2 + 1 for i in range(len(b) // 2 + 1))
 
 
-def write_test(test_file, a):
-    n = len(a)
-    b = sorted(a)
-    s = 0
-    for i in range(n // 2 + 1):
-        s += b[i] // 2 + 1
-    with open(test_file, 'w') as f:
-        f.write(str(n) + '\n')
-        f.write(' '.join(map(str, a)))
-    with open(test_file + '.a', 'w') as f:
-        f.write(str(s) + '\n')
+def question(arr):
+    return '{}\n{}\n'.format(len(arr), ' '.join(map(str, arr)))
+
+
+def answer(arr):
+    return '{}\n'.format(solution(arr))
+
+
+tests = TestSet()
+def add(arr):
+    tests.add(question(arr), answer(arr))
 
 
 if __name__ == '__main__':
-    test_folder = 'tests'
-    shutil.rmtree(test_folder, ignore_errors=True)
-    os.mkdir(test_folder)
-
-    write_test(os.path.join(test_folder, "01"), [1])
-    write_test(os.path.join(test_folder, "02"), [158])
-    write_test(os.path.join(test_folder, "03"), [1, 1, 1, 1, 1, 1, 1])
-    write_test(os.path.join(test_folder, "04"), [2, 45, 7, 6, 33])
-    write_test(os.path.join(test_folder, "05"), [random.randint(1, 100000) for _ in range(10000)])
-
-    for test in range(6, 10):
-        generate_test(os.path.join(test_folder, "0{}".format(test)))
+    seed(42)
+    for arr in ([1],
+                [158],
+                [1, 1, 1, 1, 1, 1, 1],
+                [2, 45, 7, 6, 33],
+                [randint(1, 100000) for _ in range(10000)]):
+        add(arr)
+    for _ in range(4):
+        add([randint(1, 100000) for _ in range(randint(1, 10000))])
