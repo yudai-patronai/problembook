fb7fb42c91241939aae185fd427ad252e2c64c63
Alexander Kazantsev <kazantsev@phystech.edu>
2019-11-05 21:59:05 +0300

    [sort/merge_sort] added recursion checker, updated code style

--- problems/sort/merge_sort/footer.py
+++ problems/sort/merge_sort/footer.py
@@ -1,19 +1,29 @@
-
 #''' # ЭТО не комментарий, а конец переменной source_code из header
 
 # валидация на содержание запрещённых инструкций
 exclude_patterns = [r'\s*\.\s*append', r'\s*\.\s*sort', r'sorted']
 
 for reobj in exclude_patterns:
-    assert not re.findall(reobj, source_code), 'You are using restricted constructions'
+    assert not re.findall(reobj, source_code), \
+        'You are using restricted constructions'
 
+exec(source_code)  # объект merge_sort становится доступен
 
-exec(source_code)
-
-#  считывание данных с учётом того, что могут быть пустые массивы
+# Считывание данных с учётом того, что могут быть пустые массивы
 A = list(map(int, input().split()))
+
+# Запуск функции и проверка её на inplace
 id_before = id(A)
 merge_sort(A)
 id_after = id(A)
-
 assert id_before == id_after, 'Your function does not work inplace'
+
+# Блок проверки рекурсии
+if len(A) > 1:
+    # Временное перенаправление потока вывода,
+    # потому что во время проверки рекурсии запустятся
+    # print-ы в merge_sort.
+    old_stdout, sys.stdout = sys.stdout, StringIO()
+    assert test_recursion(lambda: merge_sort(A)), \
+        'Your function must be recursive'
+    sys.stdout = old_stdout
--- problems/sort/merge_sort/solution.py
+++ problems/sort/merge_sort/solution.py
@@ -1,9 +1,9 @@
 def merge(L, R):
     len_L, len_R = len(L), len(R)
-    
+
     A = [0] * (len_L + len_R)
     ind_L = ind_R = ind_A = 0
-    
+
     while ind_L < len_L and ind_R < len_R:
         if L[ind_L] <= R[ind_R]:
             A[ind_A] = L[ind_L]
@@ -13,17 +13,17 @@ def merge(L, R):
             A[ind_A] = R[ind_R]
             ind_R += 1
             ind_A += 1
-    
+
     while ind_L < len_L:
         A[ind_A] = L[ind_L]
         ind_A += 1
         ind_L += 1
-    
+
     while ind_R < len_R:
         A[ind_A] = R[ind_R]
         ind_A += 1
         ind_R += 1
-    
+
     return A
 
 
