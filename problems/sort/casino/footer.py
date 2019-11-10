41098a4d1797e46ff005bf2b9ba0d0d79d8774b1
Alexander Kazantsev <kazantsev@phystech.edu>
2019-11-10 18:02:47 +0300

    [sort/casino] added checker of embedded sort functions and removed them from solution

---
+++ problems/sort/casino/footer.py
@@ -0,0 +1,9 @@
+#'''  # source_code переменная конец
+
+# валидация на содержание запрещённых инструкций
+exclude_patterns = [r'\s*\.\s*sort', r'sorted']
+for reobj in exclude_patterns:
+    assert not re.findall(reobj, source_code), \
+        'You are using restricted constructions'
+
+exec(source_code)
