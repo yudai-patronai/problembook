482cb09ca21ebcb25f1e0414d334ce7cbeb9f25d
Alexander Kazantsev <kazantsev@phystech.edu>
2019-11-11 23:18:24 +0300

    [recursion/matryoshka] updated restriction pattern 'for' to allow students to use '{}'.format(), e.g.

--- problems/recursion/matryoshka/footer.py
+++ problems/recursion/matryoshka/footer.py
@@ -1,10 +1,9 @@
-''' # source_code переменная конец
+#''' # source_code переменная конец
 
 # валидация на содержание запрещённых инструкций
-exclude_patterns = ['for', 'while']
+exclude_patterns = [r'for([^a-zA-Z_0-9]|$)', r'while']
 
-for pattern in exclude_patterns:
-    reobj = re.compile(pattern)
+for reobj in exclude_patterns:
     assert not re.findall(reobj, source_code), 'instruction "{}" could not be used'.format(pattern)
 
 
