5f2a155e2e0c97c48d2bcbb238a9b223ee9c970f
Степан Захаров <stepanzh@gmail.com>
2019-11-11 20:12:51 +0300

    [while/find_root] обновил задачу

--- problems/while/find_root/footer.py
+++ problems/while/find_root/footer.py
@@ -1,16 +1,11 @@
+func_str = input()
+a, b, tol, true_root = [float(e) for e in input().split()]
 
-
-#''' # ЭТО не комментарий, а конец переменной source_code из header
-
-
-exec(source_code)  # объект find_root(f, a, b, tol) становится доступным
-
-func = input()
-a, b, tol = [float(e) for e in input().split()]
 eps = 1e-6
-
-exec(func)
-
+exec(func_str)
 root = find_root(f, a, b, tol)
 
-print(root)
+if abs(f(root)) < eps or abs(root - true_root) < tol:
+    print('YES')
+else:
+    print('NO')
--- problems/while/find_root/test_generator.py
+++ problems/while/find_root/test_generator.py
@@ -1,31 +1,23 @@
 from lib.testgen import TestSet
-import math
 
-def find_root(f, a, b, tol):
-    eps = 1e-6
-    while (b - a) > tol:
-        x = (b + a) / 2.
-        val = f(x)
-        if abs(val) < eps:
-            return x
-        elif f(a) * val > 0.:
-            a = x
-        else:
-            b = x
-    return x
 
-functions = ["def f(x): return x + 0.1", "def f(x): return x * x + 5.0 * x", "def f(x): return x * x * x - 0.9", "def f(x): return math.sin(x * x * x) - 0.2"]
-a, b, tol = -1, 1, 0.01
+# function and true root
+fun_root = [
+    ("def f(x): return x + 0.1", -0.1),
+    ("def f(x): return x * x + 5.0 * x", 0.0),
+    ("def f(x): return x * x * x - 0.9", 0.9654893846056297),
+    ("def f(x): return x * x * x - 0.5", 0.7937005259840998),
+    ("def f(x): return x * x * x - 0.2", 0.5848035476425733),
+]
+
+a, b, tol = -1.0, 1.0, 0.01
 
 tests = TestSet()
 
-for el in functions:
-    question = el + "\n" + str(a) + " " + str(b) + " " + str(tol)
-    print(el)
-    exec(el)
-    answer = find_root(f, a, b, tol)
+for f_str, r in fun_root:
+    question = f_str + "\n" + "{} {} {} {}\n".format(a, b, tol, r)
 
     tests.add(
         question,
-        str(answer) + '\n'
+        'YES\n'
     )
