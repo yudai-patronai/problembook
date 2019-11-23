787cdade3943bc26e5959e2ec7ec422568683e38
Alexander Kazantsev <kazantsev@phystech.edu>
2019-11-23 15:04:09 +0300

    [strings/a_function] removed enormous-large tests but saved usual-large to avoid slow solutions pass; used TestSet; updated statement

--- problems/strings/a_function/test_generator.py
+++ problems/strings/a_function/test_generator.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 
-import os
 from lib import random
-import shutil
 import string
-
-import slow_solution
-import solution
+from lib.testgen import TestSet
 
 ALPHABET = list(string.ascii_letters)
+
+
+def compute_z_function(s):
+    z = [0]
+    left = right = 0
+    for i in range(1, len(s)):
+        x = min(z[i - left], right - i + 1) if i <= right else 0
+        while i + x < len(s) and s[x] == s[i + x]:
+            x += 1
+        if i + x - 1 > right:
+            left, right = i, i + x - 1
+        z.append(x)
+    return z
+
+
+def find_a_function(s):
+    x = s + "#" + s[::-1]
+    z = compute_z_function(x)
+    return z[:-len(s) - 1:-1]
 
 
 def generate_simple(pal, letters, l):
@@ -24,27 +39,25 @@ def generate_simple(pal, letters, l):
             step = -1
         s.append(letters[status])
         status += step
-    return "".join(s[:l])
+    return ''.join(s[:l])
 
 
-def generate_test(n, t, folder):
-    src = os.path.join(folder, "%02d" % n)
-    dst = os.path.join(folder, "%02d.a" % n)
-    with open(src, "w") as f:
-        f.write(t[0])
-    answer = solution.find_a_function(t[0])
-    if len(t) >= 2:
-        assert answer == t[1], n
-    if len(t[0]) < 1000:
-        assert answer == slow_solution.find_a_function(t[0]), n
-    with open(dst, "w") as g:
-        print(" ".join(map(str, answer)), file=g)
+def question(t):
+    return t[0]
+
+
+def answer(t):
+    res = find_a_function(t[0])
+    assert len(t) != 0 or res != t[1], 'Answers {} and {} do not match'.format(res, t[1])
+    return '{}\n'.format(' '.join(map(str, res)))
+
+
+TESTS = TestSet()
+def add_test(t):
+    TESTS.add(question(t), answer(t))
 
 
 if __name__ == "__main__":
-    test_folder = "tests"
-    shutil.rmtree(test_folder, ignore_errors=True)
-    os.mkdir(test_folder)
     random.seed(42)
     tests = [
         ("aabaa", [1, 2, 0, 1, 5]),
@@ -66,22 +79,21 @@ if __name__ == "__main__":
         (generate_simple("abcba", "xyzyxyz", 534),),
         (generate_simple(generate_simple("iki", "xyz", 5 + 6 * 3), "xyzyxyz", 1450),),
         (generate_simple(generate_simple("iki", "xyz", 5 + 6 * 3), "kixyzki", 16543),),
-        ("a" * 10000,),
+        ("a" * 20000,),
         ("abracadabr" * 23,),
         ("abracadabracad" * 23,),
-        ("a" * 200000,),
-        ("ab" * 50000,),
-        ("abcb" * 20000,),
-        ("abababcb" * 22400,),
-        ("gurugugu" * 20000,),
+        ("ab" * 10000,),
+        ("abcb" * 2000,),
+        ("abababcb" * 2240,),
+        ("gurugugu" * 2000,),
         (generate_simple("a", "xyz", 2000),),
-        (generate_simple("aba", "xyz", 20000),),
-        (generate_simple("abcba", "xyzyxyz", 200000),),
-        (generate_simple(generate_simple("iki", "xyz", 5 + 6 * 3), "xyzyxyz", 200000),),
-        (generate_simple(generate_simple("iki", "xyz", 5 + 6 * 3), "kixyzki", 200000),),
+        (generate_simple("aba", "xyz", 18000),),
+        (generate_simple("abcba", "xyzyxyz", 18000),),
+        (generate_simple(generate_simple("iki", "xyz", 5 + 6 * 3), "xyzyxyz", 18000),),
+        (generate_simple(generate_simple("iki", "xyz", 5 + 6 * 3), "kixyzki", 18000),),
         (generate_simple(generate_simple(generate_simple("aba", "abcdef", 16 + 17 * 3), "xyz", 5 + 6 * (16 + 17 * 3)),
-                         "kixyzki", 200000),),
+                         "kixyzki", 18000),),
     ]
 
-    for i, t in enumerate(tests):
-        generate_test(i + 1, t, test_folder)
+    for t in tests:
+        add_test(t)
