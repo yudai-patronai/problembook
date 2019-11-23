15d4bea4f0bf0647886a85bc2cd5ec19e4d8be4f
Vladimir Markin <vmadrid57@gmail.com>
2019-11-23 16:46:00 +0300

    strings/word_len_sort 462ef47d-bdc8-4a09-a4ef-8d8008832c5e Изменил в соответствии с новыми требованиями. В. Маркин

--- problems/strings/word_len_sort/solution.py
+++ problems/strings/word_len_sort/solution.py
@@ -1,2 +1,3 @@
-raw_sorted = sorted(input().split(' '), key=lambda x: len(x))
+raw_sorted = sorted(input().split(' '))
+raw_sorted = sorted(raw_sorted, key=lambda x: len(x))
 print(*[sum([ord(i) for i in word]) for word in raw_sorted])
--- problems/strings/word_len_sort/test_generator.py
+++ problems/strings/word_len_sort/test_generator.py
@@ -1,22 +1,18 @@
-import os
-import shutil
+from lib.testgen import TestSet
 from lib import random
 import string
 
 def solution(string):
-    raw_sorted = sorted(string.split(' '), key=lambda x: len(x))
+    raw_sorted = sorted(string.split(' '))
+    raw_sorted = sorted(raw_sorted, key=lambda x: len(x))
     return [sum([ord(i) for i in word]) for word in raw_sorted]
 
 def str_generator(size=6, chars=string.ascii_letters + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-N = 8
+N = 9
 random.seed(1984)
-
-tests_dir = os.path.join(os.path.dirname(__file__), 'tests')
-
-shutil.rmtree(tests_dir, ignore_errors=True)
-os.makedirs(tests_dir)
+tests = TestSet()
 
 for num in range(1, N + 1):
     if num == 1:
@@ -31,13 +27,10 @@ for num in range(1, N + 1):
     elif num == 4:
         word_len = random.sample(range(5, 15), 5)
         seq = ' '.join(list(map(str_generator, word_len)))
-    
+    elif num == 5:
+        seq = 'bb bbb aaa'
     else:
         word_len = random.sample(range(5, 150), 50)
         seq = ' '.join(list(map(str_generator, word_len)))
     
-    with open(os.path.join(tests_dir, '{0:0>2}'.format(num)), 'w') as f:
-        f.write(seq)
-
-    with open(os.path.join(tests_dir, '{0:0>2}.a'.format(num)), 'w') as f:
-        f.write(' '.join(map(str, solution(seq))))
+    tests.add(seq + '\n', ' '.join(map(str, solution(seq))) + '\n')
