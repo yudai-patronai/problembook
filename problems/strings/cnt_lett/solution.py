665bfc57e7aa1929f14fec28deed3744b8ad257a
Vladimir Markin <vmadrid57@gmail.com>
2019-12-07 21:45:20 +0300

    [strings/cnt_lett] Создал задачу. Владимир Маркин

---
+++ problems/strings/cnt_lett/solution.py
@@ -0,0 +1,9 @@
+instring = input()
+counters = [0] * (ord('Z') - ord('A') + 1)
+for i in range(len(instring)):
+    if instring[i] != ' ':
+        counters[ord(instring[i]) - ord('A')] += 1
+for i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+    if counters[ord(i) - ord('A')] > 0:
+        print(i, counters[ord(i) - ord('A')]
+
