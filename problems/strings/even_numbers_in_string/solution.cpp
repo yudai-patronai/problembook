290ed0d5aeb2862fb236cbb49320786807e7edcb
leonid <puleon@mail.ru>
2019-12-08 10:51:47 +0300

    fix: solution.cpp in strings/even_numbers_in_string

--- problems/strings/even_numbers_in_string/solution.cpp
+++ problems/strings/even_numbers_in_string/solution.cpp
@@ -4,14 +4,16 @@
 
 int main()
 {
-	std::string in;
-	std::getline(std::cin, in);
+  std::string in;
+  std::getline(std::cin, in);
+  int size = in.size();
+  int count = 0;
 
-	int count = 0;
-	for(int i = 0; i < in.size(); ++i)
-		if (isdigit(in[i]) && atoi(in[i]) % 2 == 0)
-			count++;
-
-	std::cout << count << std::endl;
-	return 0;
+  for (int i = 0; i < size; ++i) {
+    if (isdigit(in[i]) && static_cast<int>(in[i]) % 2 == 0) {
+      count++;
+    }
+  }
+  std::cout << count << std::endl;
+  return 0;
 }
