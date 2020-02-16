fa2000b9a07302aea3c376f25553778bca6b4064
Степан Захаров <stepanzh@gmail.com>
2020-02-16 18:54:10 +0300

    [hash/hash_table_chain_search] Задача теперь на функцию
    Переписал задачу под написание функции. Это больше подходит под
    интерфейс.
    Уменьшил размеры таблицы и хеша. Так заведомо есть коллизии и
    проще дебажить таблицу из header.
    Уменьшил кол-во тестов, убрал случайные тесты. Так проще с ними работать:
    заведомо видны KeyError тесты и положительные.
    Также в таблице значения теперь случайные строки, а не хеши.
    Иначе можно было написать хак.
    В соответствии с изменениями подробнее описал условие задачи.

--- problems/hash/hash_table_chain_search/solution.py
+++ problems/hash/hash_table_chain_search/solution.py
@@ -1,25 +1,26 @@
-def poly_hash(p, m, s):
+ERROR_MSG = 'KeyError'
 
+def poly_hash(p, m, s):
     h = 0
     for c in s:
         h = ( (h * p) % m + ord(c)) % m
-
     return h
 
 
-def search(hash_table, element):
-    length = len(hash_table)
-    m = 10000
-    p = 11
-    hsh = poly_hash(p, m, element)
-    index = hsh % length
-    if len(hash_table[index]) == 0:
-        return 'Value Not Found'
-    lenchain = len(hash_table[index])
-    for i in range(lenchain):
-        if hsh == hash_table[index][i][0]:
-            return hash_table[index][i][2]
-    return 'Value Not Found'
+def search(hash_table, key):
+    m = 100
+    p = 91
 
+    hsh = poly_hash(p, m, key)
+    chain = hash_table[hsh % len(hash_table)]
 
-print(search(hash_table1, input()))
+    if len(chain) == 0:
+        return ERROR_MSG
+    
+    lenchain = len(chain)
+    for elem in chain:
+        if hsh == elem[0] and key == elem[1]:
+            return elem[2]
+    
+    return ERROR_MSG
+
