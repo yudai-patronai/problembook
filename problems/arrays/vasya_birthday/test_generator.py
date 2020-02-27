b6ca78513785c090d9196d81adf67e3d3f705c40
stetsyuk <52718548+stetsyuk@users.noreply.github.com>
2020-02-27 20:09:19 +0300

    Fix vasya_birthday: values are unique: tests

--- problems/arrays/vasya_birthday/test_generator.py
+++ problems/arrays/vasya_birthday/test_generator.py
@@ -12,10 +12,9 @@ shutil.rmtree(test_dir, ignore_errors=True)
 os.makedirs(test_dir)
 
 for i in range(1, NUM_TEST + 1):
-    n = random.randint(1, 10000)
-    array = []
-    for j in range(n):
-        array.append(random.randint(1, 1000))
+    array = list(set([random.randint(1, 1000) for _ in range(random.randint(1, 10000))]))
+    random.shuffle(array)
+    n = len(array)
 
     with open(os.path.join(test_dir, '{0:0>2}'.format(i)), 'w') as f:
         f.write('{0}\n{1}\n'.format(n, ' '.join(str(val) for val in array)))
