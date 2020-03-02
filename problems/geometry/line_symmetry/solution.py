b1820477f2488cc937a303c79df9875dcbd3719d
ealyapina <surliza@DESKTOP-LQAQ78O.localdomain>
2020-03-02 10:00:52 +0300

    [geometry/line_symmetry] - добавлено решение

---
+++ problems/geometry/line_symmetry/solution.py
@@ -0,0 +1,20 @@
+x1, y1, x2, y2 = tuple(map(int, input().split()))
+x,  y = tuple(map(int, input().split()))
+delta_x = x2 - x1
+delta_y = y2 - y1
+if delta_x == 0:
+    x_ = 2*x1 - x
+    y_ = y
+elif delta_y == 0:
+    y_ = 2*y1 - y
+    x_ = x
+else:
+    k = delta_y / delta_y
+    b = (y1 * x2 - y2 * x1) / delta_x
+    k_perp = 1 / k
+    b_perp = y- k_perp * x
+    inter_x = (b_perp - b) / (k - k_perp)
+    inter_y = k * inter_x + b
+    x_ = 2 * inter_x - x
+    y_ = 2 * inter_y - y
+print(x_, y_)
