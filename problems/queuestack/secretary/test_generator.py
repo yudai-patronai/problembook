f1a8be94302f4d7c70f12ff46be5b5b65788afcc
Maxim Abramov <maksim.abramov@phystech.edu>
2020-03-11 01:59:03 +0300

    Renamed two problems

--- problems/queuestack/secretary/test_generator.py
+++ problems/queuestack/secretary/test_generator.py
@@ -6,7 +6,7 @@ tests_dir = os.path.join(os.path.dirname(__file__), 'tests')
 shutil.rmtree(tests_dir, ignore_errors=True)
 os.makedirs(tests_dir)
 
-a=[]
+a = []
 a.append('% 1 2 3 # % #\n')
 a.append('1 -1 # 50 % #\n')
 a.append('1 2 3 # 25 %\n')
@@ -15,7 +15,7 @@ a.append('% #\n')
 a.append('1 2 3 200 %\n')
 
 for i in range(len(a)):
-	with open(os.path.join(tests_dir, '{0:0>2}'.format(i+1)), 'w') as f:
-		f.write(a[i])
-	with open(os.path.join(tests_dir, '{0:0>2}.a'.format(i+1)), 'w') as f:
-		f.write(str(count(a[i])))
+    with open(os.path.join(tests_dir, '{0:0>2}'.format(i+1)), 'w') as f:
+        f.write(a[i])
+    with open(os.path.join(tests_dir, '{0:0>2}.a'.format(i+1)), 'w') as f:
+        f.write(str(count(a[i])))
