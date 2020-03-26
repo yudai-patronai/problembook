b0236fb5be1ae99e9e4497b29d2798f8b4bfdfea
Stiimo <maksim.abramov@phystech.edu>
2020-03-26 20:03:01 +0300

    Rewrited solution for goblins_and_shamans

--- problems/containers/goblins_and_shamans/solution.py
+++ problems/containers/goblins_and_shamans/solution.py
@@ -1,15 +1,67 @@
-from collections import deque
 from math import ceil
+
+
+def node(val):
+    return {
+        "value": val,
+        "next": None
+    }
+
+
+def queue():
+    return {
+        "size": 0,
+        "head": None,
+        "tail": None
+    }
+
+
+def enqueue(queue, val):
+    tmp = node(val)
+    if not queue["size"]:
+        queue["size"] = 1
+        queue["head"] = queue["tail"] = tmp
+        return
+    queue["size"] += 1
+    queue["tail"]["next"] = tmp
+    queue["tail"] = tmp
+
+
+def dequeue(queue):
+    val = queue["head"]["value"]
+    queue["head"] = queue["head"]["next"]
+    queue["size"] -= 1
+    if not queue["size"]:
+        queue["tail"] = None
+    return val
+
+
+def insert(queue, pos, val):
+    tmp = node(val)
+    i = 0
+    cur = queue["head"]
+    while i != pos - 1:
+        i += 1
+        cur = cur["next"]
+    tmp["next"] = cur["next"]
+    cur["next"] = tmp
+    queue["size"] += 1
+    if tmp["next"] is None:
+        queue["tail"] = tmp
+
 
 if __name__ == "__main__":
     n = int(input())
-    queue = deque()
+    queue = queue()
     for _ in range(n):
         cmd = input().split()
         if cmd[0] == "+":
-            queue.append(cmd[1])
+            enqueue(queue, cmd[1])
         elif cmd[0] == "*":
-            pos = ceil(len(queue) / 2)
-            queue.insert(pos, cmd[1])
+            pos = ceil(queue["size"] / 2)
+            if not pos:
+                enqueue(queue, cmd[1])
+            else:
+                insert(queue, pos, cmd[1])
         else:
-            print(queue.popleft())
+            print(dequeue(queue))
