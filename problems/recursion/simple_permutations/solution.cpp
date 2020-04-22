028f18694c7d7ed5cdcb2e63eea5246b420cb1dc
stetsyuk <52718548+stetsyuk@users.noreply.github.com>
2020-04-22 19:02:52 +0300

    Optimize solution for simple_permutations

--- problems/recursion/simple_permutations/solution.cpp
+++ problems/recursion/simple_permutations/solution.cpp
@@ -3,27 +3,31 @@
 
 bool prime_check(int32_t number) {
     if (number == 1) return false;
-    for (int32_t i = 2; i <= sqrt(number); ++i)
-        if (number % i == 0)
-        return false;
+    if (number == 2) return true;
+    if (!(number & 1)) return false;
+    
+    for (int32_t i = 3; i <= sqrt(number); i += 2) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
     return true;
 }
 
 void permutations(int16_t number, int16_t length, int16_t current,
-                  int16_t* buffer, bool* used, int16_t *num_of_permutations) {
+                  bool* used, int16_t *num_of_permutations, int32_t acc) {
     if (current == length) {
-        int32_t tmp = 0;
-        for (int16_t i = 0; i < length; ++i)
-            tmp += buffer[i] * pow(10, length - i - 1);
-        if (prime_check(tmp))
+        if (prime_check(acc)) {
             ++(*num_of_permutations);
+        }
     } else {
         for (int16_t variant = 0; variant < number; ++variant) {
             if (!used[variant]) {
-                buffer[current] = variant + 1;
                 used[variant] = true;
+                acc *= 10; acc += variant + 1;
                 permutations(number, length, current + 1,
-                             buffer, used, num_of_permutations);
+                             used, num_of_permutations, acc);
+                acc /= 10;
                 used[variant] = false;
             }
         }
@@ -34,15 +38,12 @@ void permutations(int16_t number, int16_t length, int16_t current,
 int main() {
     int16_t n, m;
     std::cin >> n >> m;
-    int16_t* buffer = new int16_t[m];
-    bool* used = new bool[n];
-    for (int16_t i = 0; i < n; ++i)
+    bool used[10];
+    for (int16_t i = 0; i < n; ++i) {
         used[i] = false;
+    }
     int16_t num_of_permutations = 0;
-    permutations(n, m, 0, buffer, used, &num_of_permutations);
+    permutations(n, m, 0, used, &num_of_permutations, 0);
     std::cout << num_of_permutations << std::endl;
-    delete[] buffer;
-    delete[] used;
     return 0;
 }
-
