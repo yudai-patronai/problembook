16630fdda630c113ef070f1b34239305c7c0e835
Степан Захаров <stepanzh@gmail.com>
2020-09-05 19:31:33 +0300

    [intro_exam/prog_tasks/sort_3_numbers] генератор тестов

--- problems/intro_exam/prog_tasks/sort_3_numbers/test_generator.py
+++ problems/intro_exam/prog_tasks/sort_3_numbers/test_generator.py
@@ -1,21 +1,25 @@
 from lib.testgen import TestSet
-from lib.random import randint
+from lib.random import randint, seed
+
+seed(18)
 
 
 def question(arr):
     return '\n'.join(map(str, arr)) + '\n'
 
-def ans(arr):
+def answer(arr):
     return ' '.join(map(str, arr)) + '\n'
 
 def random_arr():
-    return [randint(-100, 100) for _ range(3)]
+    return [randint(-100, 100) for _ in range(3)]
 
 tests = TestSet()
 
-for _ in range(10):
-    arr = gen_arr()
+for _ in range(9):
+    arr = random_arr()
 
     arr_sorted = sorted(arr)
 
-    tests.add(question(arr), ans(arr_sorted))
+    tests.add(question(arr), answer(arr_sorted))
+
+tests.add(question([10, -8, -8]), answer([-8, -8, 10]))
