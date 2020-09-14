59da721e29dfeac269bd672a40bbf8d2a187b525
Stetsyuk Vladislav <admin@kustosoft.com>
2020-09-14 20:06:26 +0300

    Add classes/fractions problem

---
+++ problems/classes/fractions/solution.cpp
@@ -0,0 +1,96 @@
+#include <cstdint>
+
+namespace {
+    inline int64_t gcd(int64_t a, uint64_t b) {
+        if (a < 0) a = -a;
+        while (b > 0) {
+            a %= b;
+            a ^= b;
+            b = a ^ b;
+            a ^= b;
+        }
+        return a;
+    }
+}  //  namespace
+
+class Fraction {
+private:
+    int64_t numerator;
+    uint64_t denominator;
+
+    void Simplify() {
+        int64_t cd = gcd(numerator, denominator);
+        numerator /= cd;
+        denominator /= cd;
+    }
+
+    template<class T>
+    friend bool operator==(const Fraction& lhs, const T& rhs);
+
+public:
+    Fraction() = delete;
+
+    Fraction(const Fraction& rhs)
+    : numerator(rhs.numerator), denominator(rhs.denominator) {}
+
+    Fraction& operator=(const Fraction& rhs) {
+        numerator = rhs.numerator;
+        denominator = rhs.denominator;
+        return *this;
+    }
+
+    Fraction(int64_t num, uint64_t denom)
+    : numerator(num), denominator(denom) {
+        Simplify();
+    }
+    explicit Fraction(int64_t num) : numerator(num), denominator(1) {}
+
+    Fraction& operator+=(const Fraction& rhs) {
+        uint64_t cd = gcd(denominator, rhs.denominator);
+        numerator = (numerator * (rhs.denominator / cd)) +
+                    (rhs.numerator * (denominator / cd));
+        denominator *= (rhs.denominator / cd);
+
+        Simplify();
+
+        return *this;
+    }
+
+    Fraction operator+(const Fraction& rhs) const {
+        Fraction t(*this);
+        t += rhs;
+        return t;
+    }
+
+    Fraction operator-() const {
+        return {-numerator, denominator};
+    }
+
+    Fraction& operator-=(const Fraction& rhs) {
+        (*this) += (-rhs);
+        return *this;
+    }
+
+    Fraction operator-(const Fraction& rhs) const {
+        return (*this) + (-rhs);
+    }
+
+    Fraction& operator*=(const Fraction& rhs) {
+        auto cd1 = gcd(rhs.numerator, denominator);
+        auto cd2 = gcd(numerator, rhs.denominator);
+
+        numerator /= cd2;
+        numerator *= (rhs.numerator / cd1);
+
+        denominator /= cd1;
+        denominator *= (rhs.denominator / cd2);
+
+        return *this;
+    }
+
+    Fraction operator*(const Fraction& rhs) const {
+        Fraction t(*this);
+        t *= rhs;
+        return t;
+    }
+};
---
+++ problems/classes/fractions/statement.md
@@ -0,0 +1,37 @@
+---
+id: 0d164571-bc82-4467-86a6-efa582790944
+longname: Дроби
+languages: [cpp]
+tags: [classes]
+checker: cmp_int_seq
+source_header: header.cpp
+source_footer: footer.cpp
+time_limit: 1
+real_time_limit: 1
+max_vm_size: 64M
+---
+
+
+Напишите класс, который будет представлять рациональное число в виде дроби. Реализуйте в нем операторы сложения, вычитания, умножения и унарный минус. Используйте заготовку, приведенную ниже.
+Важно: думать о входных или выходных данных не нужно, отправьте только реализацию класса.
+
+```
+class Fraction {
+private:
+    // Do NOT rename
+    int64_t numerator;
+    uint64_t denominator;
+
+    // Do NOT delete
+    template<class T>
+    friend bool operator==(const Fraction& lhs, const T& rhs);
+
+public:
+    Fraction() = delete;
+    Fraction(const Fraction& rhs) {}
+    Fraction& operator=(const Fraction& rhs);
+    Fraction(int64_t numerator, uint64_t denominator);
+    //  Add operator overloads below
+};
+
+```
