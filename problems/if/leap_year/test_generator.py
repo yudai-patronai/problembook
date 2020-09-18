839571451f05d47499b65776972af6ccddc07aae
Степан Захаров <stepanzh@gmail.com>
2020-09-18 19:57:30 +0300

    [if/leap_year] Тесты: ограничение в 10 тестов и lib.testgen

--- problems/if/leap_year/test_generator.py
+++ problems/if/leap_year/test_generator.py
@@ -1,44 +1,17 @@
-#!/usr/bin/env python3
-
-import os
-import shutil
 import random
+from lib.testgen import TestSet
 
 random.seed(100)
-tests_dir = os.path.join(os.path.dirname(__file__), 'tests')
 
+def solution(n:int):
+    return ((n % 4 == 0) and (n % 100 != 0)) or (n % 400 == 0)
 
-def gen_test(tests_dir, ind, n):
-    test = os.path.join(tests_dir, '%.2d' % ind)
-    ans = test + '.a'
+def question(n:int):
+    return str(n) + '\n'
 
-    with open(test, 'w') as f:
-        f.write(str(n) + '\n')
+def answer(b:bool):
+    return 'YES\n' if b else 'NO\n'
 
-    with open(ans, 'w') as f:
-        if ((n % 4 == 0) and (n % 100 != 0)) or (n % 400 == 0):
-            f.write('YES\n')
-        else:
-            f.write('NO\n')
-
-
-def gen_tests(tests_dir):
-    shutil.rmtree(tests_dir, ignore_errors=True)
-    os.makedirs(tests_dir)
-
-    gen_test(tests_dir, 1, 1)
-    gen_test(tests_dir, 2, 2000)
-    gen_test(tests_dir, 3, 400)
-    t = 4
-    for i in range(10):
-        gen_test(tests_dir, t, random.randrange(1, 50000))
-        t += 1
-        gen_test(tests_dir, t, random.randrange(4, 50000, 4))
-        t += 1
-        gen_test(tests_dir, t, random.randrange(100, 50000, 100))
-        t += 1
-        gen_test(tests_dir, t, random.randrange(400, 50000, 400))
-        t += 1
-
-
-gen_tests(tests_dir)
+tests = TestSet()
+for n in 1, 2000, 400, 4, 100, 500, 1260, 1200, 791, 800:
+    tests.add(question(n), answer(solution(n)))
