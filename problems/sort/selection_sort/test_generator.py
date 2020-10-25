5564d9cb5c13b05930fc79b0b0ae037064f9c76e
Степан Захаров <stepanzh@gmail.com>
2020-10-25 12:41:50 +0300

    [sort selection_sort] обновление задачи

--- problems/sort/selection_sort/test_generator.py
+++ problems/sort/selection_sort/test_generator.py
@@ -1,39 +1,43 @@
-#!/usr/bin/python3
-
-import os
-import shutil
+from lib.testgen import TestSet
 from lib import random
 
 NUM_TEST = 10
 random.seed(10000)
 
-test_dir = os.path.join(os.path.dirname(__file__), 'tests')
-shutil.rmtree(test_dir, ignore_errors=True)
-os.makedirs(test_dir)
 
 def selection_sort(a):
-    outputs = []
-    str_a  = ' '.join(map(str, a))
+    a_states = []
+    a_states.append(' '.join(map(str, a)))
     for i in range(0, len(a) - 1):
         min_j = i
         for j in range(i, len(a)):
             if a[j] < a[min_j]:
                 min_j = j
         a[i], a[min_j] = a[min_j], a[i]
-        str_a_new = ' '.join(map(str, a))
-        if str_a_new != str_a:
-            str_a = str_a_new
-            outputs.append(str_a)
-    return outputs
+        a_states.append(' '.join(map(str, a)))
+    return a_states
 
-for i in range(1, NUM_TEST + 1):
-    n = random.randint(1, 10)
+
+def add_test(tests:TestSet, array):
+    a = array.copy()
+    question = ' '.join(map(str, a)) + '\n'
+    sort_states = selection_sort(a)
+    answer = '\n'.join(sort_states) + '\n'
+    tests.add(question, answer)
+
+
+tests = TestSet()
+
+manual = [
+    [3, 1, -1, 2],
+    [3, 1, 2, 2, -1],
+    [8, -12, 94, 23, 12, -18]
+]
+
+for array in manual:
+    add_test(tests, array)
+
+for i in range(NUM_TEST-len(manual)):
+    n = random.randint(10, 20)
     array = [random.randint(-1000, 1000) for _ in range(n)]
-
-    with open(os.path.join(test_dir, '{0:0>2}'.format(i)), 'w') as f:
-        f.write('{0}\n'.format(' '.join(str(val) for val in array)))
-
-    with open(os.path.join(test_dir, '{0:0>2}.a'.format(i)), 'w') as f:
-        outputs = selection_sort(array)
-        for el in outputs:
-            f.write('{0}\n'.format(el))
+    add_test(tests, array)
