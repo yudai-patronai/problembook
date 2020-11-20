0de3ea802edd623e948093fdbf67679b71a8267f
Степан Захаров <stepanzh@gmail.com>
2020-11-20 18:32:10 +0300

    [dynamic 1d grasshopper] удалил тест с неуникальным решением, поддержки пока нет

--- problems/dynamic/1d/grasshopper_path/test_generator.py
+++ problems/dynamic/1d/grasshopper_path/test_generator.py
@@ -1,5 +1,5 @@
 from lib.testgen import TestSet
-from lib.random import seed, randomint
+from lib.random import seed, randint
 from solution import solution
 
 seed(42)
@@ -11,14 +11,14 @@ def to_string(n, A):
 def random_array(lenA):
     A = [None] * lenA
     for i in range(lenA):
-        A[i] = randomint(1, 100)  # for boundaries see statement.md
+        A[i] = randint(1, 100)  # for boundaries see statement.md
     return A
 
 
 tests = TestSet()
 tests.add(to_string(3, [1, 3, 1]), to_string(2, [0, 2]))
 tests.add(to_string(4, [2, 1, 5, 1]), to_string(2, [1, 3]))
-tests.add(to_string(4, [1, 1, 1, 1]), to_string(2, [1, 3]))
+# tests.add(to_string(4, [1, 1, 1, 1]), to_string(2, [1, 3]))  # более одного решения
 tests.add(to_string(6, [100, 1, 1, 100, 1, 1]), to_string(4, [1, 2, 4, 5]))
 
 # random tests
