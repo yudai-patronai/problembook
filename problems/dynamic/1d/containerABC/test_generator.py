60fb6f742aa35545325bbedb206546f5b2305cb1
Maxim Abramov <maxim.abramov@s-telematics.ru>
2020-12-14 12:19:51 +0300

    Using another solution in testgen in caontainerABC

--- problems/dynamic/1d/containerABC/test_generator.py
+++ problems/dynamic/1d/containerABC/test_generator.py
@@ -3,10 +3,19 @@
 import os
 from lib import random
 import shutil
-from solution import solve
 
 random.seed(42)
 MAX_N = 20
+
+
+def solve(n):
+    d = [[None] * 3 for _ in range(n)]
+    d[0] = [1, 1, 1]
+    for i in range(1, n):
+        d[i][0] = d[i-1][1] + d[i-1][2]
+        d[i][1] = d[i][2] = sum(d[i-1])
+    return(sum(d[-1]))
+
 
 def generate_test(name, n):
     with open(name, "w") as f:
@@ -14,6 +23,7 @@ def generate_test(name, n):
     #answer
     with open("%s.a" % name, 'w') as f:
         f.write(str(solve(n)))
+
 
 if __name__ == "__main__":
     test_folder = "tests"
