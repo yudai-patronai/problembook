0faae78fd0e516db38b023417c70023b81b2225c
Maxim Abramov <maksim.abramov@phystech.edu>
2021-02-24 12:43:43 +0300

    Fixed codestyle

--- problems/hash/unique_data/solution.py
+++ problems/hash/unique_data/solution.py
@@ -2,4 +2,4 @@ if __name__ == "__main__":
     input()
     a = set(map(int, input().split()))
     b = set(map(int, input().split()))
-    print(len(a), len(b), len(a&b))
+    print(len(a), len(b), len(a & b))
