4e27744e7cb420dcefba3c9944ca00f1e1313caf
Степан Захаров <stepanzh@gmail.com>
2021-05-10 18:24:17 +0300

    [graphs dijkstra dijkstra_len] решение без использования библиотечных lib.graph функций

---
+++ problems/graphs/dijkstra/dijkstra_len/solution_clean.py
@@ -0,0 +1,38 @@
+import heapq
+
+
+def dijkstra_distance(G, start, end):
+    inf = float('inf')
+    visited = {u: False for u in G}
+    distance = {u: inf for u in G}
+
+    distance[start] = 0
+    pqueue = [(distance[u], u) for u in G]
+    heapq.heapify(pqueue)
+
+    while pqueue:
+        distmin, umin = heapq.heappop(pqueue)
+        if visited[umin]:
+            continue
+        if distmin == inf:
+            break
+        for v, w in G[umin]:
+            if distance[v] > distance[umin] + w:
+                distance[v] = distance[umin] + w
+                heapq.heappush(pqueue, (distance[v], v))
+        visited[umin] = True
+    return distance[end]
+
+
+V, E, start, end = input().split()
+start, end = map(int, (start, end))
+
+V, E = map(int, (V, E))
+G = {u: set() for u in range(V)}
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    G[u].add((v, w))
+    G[v].add((u, w))
+
+d = dijkstra_distance(G, start, end)
+print(d)
