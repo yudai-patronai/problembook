adffef70334368a23a51d77876846d0fac33d311
Степан Захаров <stepanzh@gmail.com>
2021-05-10 13:16:57 +0300

    [graphs dijkstra dijkstra_len] уменьшил число тестов с 50 до 25
    Пришлось скопировать функцию из lib.graphs и хардкодно :c уменьшить число тестов.
    
    C'est la vie.

--- problems/graphs/dijkstra/dijkstra_len/test_generator.py
+++ problems/graphs/dijkstra/dijkstra_len/test_generator.py
@@ -1,9 +1,42 @@
 #!/usr/bin/env python3
 
-import os
+import os, shutil
 
 from lib.graphs import task
+from lib import random
+
+
+# пришлось скопировать функцию из lib.graphs и хардкодно уменьшить число тестов
+def gen_tests_weight_xy(tests_dir):
+    shutil.rmtree(tests_dir, ignore_errors=True)
+    os.makedirs(tests_dir)
+
+    t = 1
+    for i in range(5):
+        n = 2 + i * 2
+        task.gen_test_weight(tests_dir, t, n, i)
+        t += 1
+
+    for i in range(5):
+        n = 2 + i * 2
+        task.gen_test_weight(tests_dir, t, n, 0)
+        t += 1
+
+    for i in range(5):
+        n = random.randrange(100, 1000)
+        task.gen_test_weight(tests_dir, t, n, 5)
+        t += 1
+
+    for i in range(2):
+        n = random.randrange(100, 1000)
+        task.gen_test_weight_xy(tests_dir, t, n, 10, 5, 5)
+        t += 1
+
+    for i in range(8):
+        n = random.randrange(100, 1000)
+        task.gen_test_weight(tests_dir, t, n, 100)
+        t += 1
+
 
 tests_dir = os.path.join(os.path.dirname(__file__), 'tests')
-
-task.gen_tests_weight_xy(tests_dir)
+gen_tests_weight_xy(tests_dir)
