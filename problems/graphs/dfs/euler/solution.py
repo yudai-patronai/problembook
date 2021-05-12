b7f0c07ef5309c9b7e01232658005d9ebcb22c12
Maxim Abramov <maxim.abramov@s-telematics.ru>
2021-05-12 14:34:41 +0300

    Moved euler to dfs subfolder

---
+++ problems/graphs/dfs/euler/solution.py
@@ -0,0 +1,35 @@
+from queue import SimpleQueue
+
+def bfs(adj_list):
+    n = len(adj_list)
+    used = [0] * n
+    used[0] = 1
+    q = SimpleQueue()
+    q.put(0)
+    while not q.empty():
+        u = q.get()
+        for v in adj_list[u]:
+            if not used[v]:
+                used[v] = 1
+                q.put(v)
+    return sum(used) == n
+
+def solve(adj_list):
+    for i in range(n):
+        if len(adj_list[i]) % 2:
+            return "NO"
+    if bfs(adj_list):
+        return "YES"
+    else:
+        return "NO"
+
+
+if __name__ == "__main__":
+    n, m = map(int, input())
+
+    adj_list = [set() for _  in range(n)]
+    for _ in range(m):
+        u, v = map(int, input().split())
+        adj_list[u].add(v)
+        adj_list[v].add(u)
+    print(solve(adj_list))
---
+++ problems/graphs/dfs/euler/test_generator.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+import random
+import shutil
+import os
+from lib.graphs import task
+
+
+simple_tests = [
+    ("""4 4
+0 1
+1 2
+2 3
+3 0
+""", "YES\n"),
+
+    ("""3 2
+0 1
+1 2
+""", "NO\n"),
+
+    ("""1
+0
+""", "YES\n"),
+
+    ("""2 1
+0 1
+""", "NO\n"),
+
+    ("""6 6
+0 1
+2 1
+0 2
+3 4
+4 5
+5 3
+""", "NO\n"),
+
+    ("""6 7
+0 1
+2 1
+0 2
+3 4
+4 5
+5 3
+0 5
+""", "NO\n"),
+
+    ("""6 7
+0 1
+2 1
+0 2
+3 4
+4 5
+5 2
+2 3
+""", "YES\n"),
+]
+
+tests_dir = os.path.join(os.path.dirname(__file__), 'tests')
+shutil.rmtree(tests_dir, ignore_errors=True)
+os.makedirs(tests_dir)
+
+t = 1
+for q, a in simple_tests:
+    test = os.path.join(tests_dir, '%.2d' % t)
+    ans = test + '.a'
+    with open(test, 'w') as f:
+        f.write(q + "\n")
+
+    with open(ans, 'w') as f:
+        f.write(a)
+    t += 1
+
+for i in range(5):
+    n = 2 + i * 2
+    task.gen_test(tests_dir, t, n, task.gen_graph_edges(n, i))
+    t += 1
+
+for i in range(5):
+    n = 4 + i * 2
+    task.gen_test(tests_dir, t, n, task.gen_graph_edges(n, 0))
+    t += 1
+
+for i in range(5):
+    n = random.randrange(100, 1000)
+    task.gen_test(tests_dir, t, n, task.gen_graph_edges(n, 100))
+    t += 1
+
+for i in range(5):
+    n = random.randrange(100, 1000)
+    task.gen_test(tests_dir, t, n, task.gen_graph_edges(n, 10000))
+    t += 1
